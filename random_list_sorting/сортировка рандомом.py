f44729d2ea541a8a2c05ab51bbda0ec963fb1f90
Anezyco <smq892675@gmail.com>
2025-03-11 22:53:37 +0500

    Update сортировка рандомом.py

--- random_list_sorting/сортировка рандомом.py
+++ random_list_sorting/сортировка рандомом.py
@@ -4,12 +4,12 @@ start_time = time.time()
 number = list((input("Введите число для сортировки: ")))
 sort_number = ''
 cnt = 0
-while sort_namber == '':
+while sort_number == '':
     num = -1000000
     for i in range(len(number)):
             if int(number[i]) >= num:
                 num = int(number[i])
-                sort_namber += number[i]
+                sort_number += number[i]
             else:
                 sort_number = ''
                 shuffle(number)
