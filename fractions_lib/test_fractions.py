60549772d9c295085930610ea9cbb826697b2020
Anezyco <smq892675@gmail.com>
2025-03-15 03:25:11 +0500

    тест библиотеки

---
+++ fractions_lib/test_fractions.py
@@ -0,0 +1,13 @@
+from fractions import sumf,diff,timesf,sharef
+
+num,den = sumf(1,2,1,2)
+print(num,den)
+
+num,den = diff(12,24,54,24)
+print(num,den)
+
+num,den = timesf(5,7,2,9)
+print(num,den)
+
+num,den = sharef(3,6,8,2)
+print(num,den)
